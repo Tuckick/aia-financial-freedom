e3639e908e626a0e41e36414ea20f3b3fe361156
Tuck <tuk.pensrisirikul@hotmail.com>
2021-12-06 14:34:48 +0700

    init scss for project

---
+++ components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="header-container">
+    <img
+      src="../assets/logo/financial-freedom-logo.png"
+      alt="financial freedom logo"
+      class="logo-container"
+    />
+  </section>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+@import '~assets/scss/variables';
+
+.header-container {
+  background-color: $dark-gray;
+}
+.logo-container {
+  height: 100px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <div>My blog navigation bar here</div>
+  <div class="app-container">
+    <Header id="header-section" />
     <Nuxt />
   </div>
 </template>
+<style lang="scss">
+.app-container {
+  margin: 0;
+  padding: 0;
+}
+</style>
