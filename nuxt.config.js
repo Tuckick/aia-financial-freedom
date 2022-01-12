3578f43afe8163cbfeae02401e2e5d2b9a76efa5
Tuck <tuk.pensrisirikul@hotmail.com>
2022-01-12 16:37:01 +0700

    install Sukhumvit font

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/scss/_variables.scss'],
+  css: ['~/assets/scss/_variables.scss', '~layouts/global.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
