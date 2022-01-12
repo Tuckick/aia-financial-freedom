6a577e35a3c09666b914600a47d3f3da13e84f4e
Tuck <tuk.pensrisirikul@hotmail.com>
2022-01-12 18:58:28 +0700

    implement products and services section in homepage

---
+++ components/pages/home/productNservice.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="products-and-services-container">
+        <section class="product-section">
+            
+        productNservice
+        </section>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ProductsAndServices'
+}
+</script>
+<style lang="scss" scoped>
+.products-and-services-container {
+    margin: 0 -20px;
+    .product-section{
+        background-color: aquamarine;
+    }
+}
+</style>
